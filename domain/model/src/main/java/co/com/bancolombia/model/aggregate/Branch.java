688b670591dd0a8465283f1cadf97fc484d98e31
iuribea <iuribea@proteccion.com.co>
2026-09-20 06:27:37 -0500

    refactor(model): expose public all-args constructors

--- domain/model/src/main/java/co/com/bancolombia/model/aggregate/Branch.java
+++ domain/model/src/main/java/co/com/bancolombia/model/aggregate/Branch.java
@@ -1,5 +1,7 @@
 package co.com.bancolombia.model.aggregate;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
@@ -8,6 +10,7 @@ import java.util.List;
 
 @Value
 @Builder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Branch {
     String id;
     String name;
--- domain/model/src/main/java/co/com/bancolombia/model/aggregate/Product.java
+++ domain/model/src/main/java/co/com/bancolombia/model/aggregate/Product.java
@@ -1,10 +1,13 @@
 package co.com.bancolombia.model.aggregate;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Product {
     String id;
     String name;
