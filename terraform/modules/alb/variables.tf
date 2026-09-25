ea2e303444142d8783e9138c1139b48c65d6f7bd
iuribea <iuribea@proteccion.com.co>
2026-09-24 20:41:23 -0500

    build(terraform): add ALB module

---
+++ terraform/modules/alb/variables.tf
@@ -0,0 +1,16 @@
+variable "project_name" {
+  type    = string
+  default = "franchise"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "alb_security_group_id" {
+  type = string
+}
